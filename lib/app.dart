06a50ccf1536e5a0679ae3a6e03a6d9555e2d530
Dominik <thedolien@gmail.com>
2022-10-28 13:29:03 +0200

    body optimizer

---
+++ lib/app.dart
@@ -0,0 +1,13 @@
+import 'package:body_optimizer/screens/homepage.dart';
+import 'package:flutter/material.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: MyHomePage(),
+    );
+  }
+}
