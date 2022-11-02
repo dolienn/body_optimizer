8eac79af280ed32775fa8a630e62969c54ea1e9b
KamilFenne <ludwikowski.kamilxbox360@gmail.com>
2022-11-02 09:45:57 +0100

    zmiany

--- lib/app.dart
+++ lib/app.dart
@@ -1,5 +1,5 @@
 import 'package:body_optimizer/screens/homepage.dart';
-import 'package:body_optimizer/screens/settings.dart';
+import 'package:body_optimizer/screens/mainpage.dart';
 import 'package:flutter/material.dart';
 
 class MyApp extends StatelessWidget {
@@ -8,8 +8,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: SettingsPage(),
+      home: MainPage(),
     );
   }
 }
