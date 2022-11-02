17ac350c8061a61a533ec2e8dadf59cf054eca77
Dominik <thedolien@gmail.com>
2022-11-02 09:14:29 +0100

    no update lekki, teraz dziala

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -9,7 +9,6 @@ import 'package:body_optimizer/app.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:body_optimizer/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
