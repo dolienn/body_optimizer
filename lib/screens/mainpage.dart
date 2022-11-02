cd630922ea88c1cf558a922fdf7de4b9a8983088
KamilFenne <ludwikowski.kamilxbox360@gmail.com>
2022-11-02 09:46:08 +0100

    form list view usuwanie const w main page etc

--- lib/screens/mainpage.dart
+++ lib/screens/mainpage.dart
@@ -2,9 +2,7 @@ import 'package:body_optimizer/screens/homepage.dart';
 import 'package:body_optimizer/screens/settings.dart';
 import 'package:body_optimizer/screens/workout.dart';
 import 'package:body_optimizer/screens/workoutcreator.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:persistent_bottom_nav_bar/persistent_tab_view.dart';
 
 class MainPage extends StatefulWidget {
   const MainPage({Key? key}) : super(key: key);
@@ -13,84 +11,41 @@ class MainPage extends StatefulWidget {
   State<MainPage> createState() => _MainPageState();
 }
 
-
 class _MainPageState extends State<MainPage> {
-  final PersistentTabController _controller =
-      PersistentTabController(initialIndex: 0);
-
-  List<Widget> _buildScreens() {
-    return [
-      const MyHomePage(),
-      const Workout(),
-      const WorkoutCreator(),
-      const Settings(),
-    ];
-  }
-
-  List<PersistentBottomNavBarItem> _navBarsItems() {
-    return [
-      PersistentBottomNavBarItem(
-        icon: const Icon(CupertinoIcons.home),
-        title: ("Home"),
-        activeColorPrimary: PublicVariables().activeColor,
-        inactiveColorPrimary: CupertinoColors.systemGrey,
-      ),
-      PersistentBottomNavBarItem(
-        icon: const Icon(Icons.fitness_center_outlined),
-        title: ("Workout"),
-        activeColorPrimary: PublicVariables().activeColor,
-        inactiveColorPrimary: CupertinoColors.systemGrey,
-      ),
-      PersistentBottomNavBarItem(
-        icon: const Icon(Icons.note_add_outlined),
-        title: ("Workout Creator"),
-        activeColorPrimary: PublicVariables().activeColor,
-        inactiveColorPrimary: CupertinoColors.systemGrey,
-      ),
-      PersistentBottomNavBarItem(
-        icon: const Icon(Icons.settings_outlined),
-        title: ("Settings"),
-        activeColorPrimary: PublicVariables().activeColor,
-        inactiveColorPrimary: CupertinoColors.systemGrey,
-      ),
-    ];
+  int currentIndex = 0;
+  List pages = [const MyHomePage(), const Workout(), WorkoutCreator(), const Settings()];
+  void onTap(int index) {
+    setState(() {
+      currentIndex = index;
+    });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: PersistentTabView(
-        context,
-        controller: _controller,
-        screens: _buildScreens(),
-        items: _navBarsItems(),
-        confineInSafeArea: true,
-        backgroundColor: Colors.white, // Default is Colors.white.
-        handleAndroidBackButtonPress: true, // Default is true.
-        resizeToAvoidBottomInset:
-            true, // This needs to be true if you want to move up the screen when keyboard appears. Default is true.
-        stateManagement: true, // Default is true.
-        hideNavigationBarWhenKeyboardShows:
-            true, // Recommended to set 'resizeToAvoidBottomInset' as true while using this argument. Default is true.
-        decoration: NavBarDecoration(
-          borderRadius: BorderRadius.circular(10.0),
-          colorBehindNavBar: Colors.white,
-        ),
-        popAllScreensOnTapOfSelectedTab: true,
-        popActionScreens: PopActionScreensType.all,
-        itemAnimationProperties: const ItemAnimationProperties(
-          // Navigation Bar's items animation properties.
-          duration: Duration(milliseconds: 200),
-          curve: Curves.ease,
-        ),
-        screenTransitionAnimation: const ScreenTransitionAnimation(
-          // Screen transition animation on change of selected tab.
-          animateTabTransition: true,
-          curve: Curves.ease,
-          duration: Duration(milliseconds: 200),
-        ),
-        navBarStyle:
-            NavBarStyle.style4, // Choose the nav bar style with this property.
+      body: pages[currentIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        selectedFontSize: 0,
+        unselectedFontSize: 0,
+        type: BottomNavigationBarType.fixed,
+        backgroundColor: Colors.deepPurple.shade900,
+        currentIndex: currentIndex,
+        onTap: (index) => setState(() => currentIndex = index),
+        selectedItemColor: Colors.white,
+        unselectedItemColor: Colors.grey,
+        showSelectedLabels: false,
+        showUnselectedLabels: false,
+        elevation: 0,
+        items: const [
+          BottomNavigationBarItem(
+              label: "Home", icon: Icon(Icons.home)),
+          BottomNavigationBarItem(
+              label: "Workout", icon: Icon(Icons.fitness_center)),
+          BottomNavigationBarItem(
+              label: "Workout Creator", icon: Icon(Icons.create)),
+          BottomNavigationBarItem(
+              label: "Settings", icon: Icon(Icons.settings))
+        ],
       ),
     );
   }
--- lib/screens/workoutcreator.dart
+++ lib/screens/workoutcreator.dart
@@ -1,7 +1,30 @@
+/*
+do zrobienia:
+nazwa cwiczenia
+możliwość dodawania ćwiczeń
+usuwanie ćwiczeń
+wejscie w drugi ekran w ktorym sa kategorie cwiczen
+nastepnie lista cwiczen
+wybieranie cwiczen i wstawiania
+
+
+trudne to bardzo :(
+ */
 import 'package:flutter/material.dart';
 
+class zmienne{
+  var iloscCwiczen = 0; //liczba jest dodawana kiedy uzytkownik dodaje cwiczenia
+  // jest uzywana do tworzenia cwiczen w liscie
+}
+
+//moze tworzyc klase o danych cwicenia ??? class cw nr 1 za pomoca jakiejs pentli a nastepnie var tytul var opis var link do zdj
+
 class WorkoutCreator extends StatelessWidget {
-  const WorkoutCreator({Key? key}) : super(key: key);
+  WorkoutCreator({Key? key}) : super(key: key);
+
+  final _formKey = GlobalKey<FormState>();
+
+
 
   @override
   Widget build(BuildContext context) {
@@ -9,27 +32,65 @@ class WorkoutCreator extends StatelessWidget {
       title: '_title',
       home: Scaffold(
         appBar: AppBar(title: const Text('Create custom workout'), centerTitle: true, backgroundColor: Colors.deepPurple.shade900),
-        body:  Container(
+        body: Column(
+          children: [
+            Form(
+               key: _formKey,
+               child: Column(
+                 children: [
+                   TextFormField(
+                     //jakoś text w tym wyśrodkować
+                     decoration: const InputDecoration(
+                     //icon: Icon(Icons.person),
+                     hintText: 'Custom workout name'
+                   ),
+                     onSaved: (String? value) {
+                       // This optional block of code can be used to run
+                       // code when the user saves the form.
+                     },
+                     //tutaj dać, że nazwa workoutu nie może być pusta (oraz może, że nie może być dłuższa niż x znaków
+                     validator: (String? value) {
+                       return (value != null && value.contains('@')) ? 'Workout name must not be empty!' : null;
+                     },
+                   ),
+                   //ten przycisk dać na sam dół do zapisywania wszystkiego na tej stronie
+                   ElevatedButton(
+                     style: ButtonStyle(
+                       backgroundColor: MaterialStatePropertyAll<Color>(Colors.deepPurple.shade900),
+                     ),
+                     //onPressed: zapisz cały workout
+                     onPressed: () {},
+                     child: const Text('Create'),
+                   ),
+                   //ten przycisk dać obok zamiast pod
+                    OutlinedButton(onPressed: () {}, child: const Text('Cancel', style: TextStyle(color: Colors.deepPurple),)),
+                 ],
+             ),
 
-          child: ListView(
+          //tutaj jakos form dac dla nazwy workoutu
+          /*ListView(
             scrollDirection: Axis.vertical,
             children: <Widget>[
               //tutaj będą dodawane ćwiczenia od klienta
+              //dać jakieś id aby móc dodawać tutaj container list tile?
               Container(
                 width: 150.0,
                 color: Colors.black12,
                 child: Stack(
                   children: <Widget>[
                     ListTile(
-                      title: Text('Home'),
+                      leading: Icon(Icons.recycling), //znalesc kosz
+                      title: Text('Exercise'),
                       trailing: Icon(Icons.notes),
                     ),
                   ],
                 ),
               ),
-            ],
+            ],*/
           ),
-
+          ],
+          //tutaj przyciski dodaj cwiczenie, odrzuc/zapisz zmiany
+          //nastepnie strony do dodawania cwiczen
         ),
 
 
@@ -43,3 +104,4 @@ class WorkoutCreator extends StatelessWidget {
 
 
 
+//a
