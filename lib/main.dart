21023ccc94538212757c2e4b511c45ead6cba0e3
silentjacob <kubulus2004@tlen.pl>
2022-11-03 13:03:22 +0100

    made project from scratch again :(

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
+// ignore: depend_on_referenced_packages
 import 'package:intl/date_symbol_data_local.dart';
 import 'app.dart';
 
 void main() {
-  initializeDateFormatting().then((_) => runApp(const MainPage()));
+  initializeDateFormatting().then((_) => runApp(const MyApp()));
 }
-
 
--- lib/screens/homepage.dart
+++ lib/screens/homepage.dart
@@ -1,101 +1,133 @@
 import 'package:flutter/material.dart';
 import 'package:table_calendar/table_calendar.dart';
+import 'package:persistent_bottom_nav_bar/persistent_tab_view.dart';
 
-
-class HomePage extends StatefulWidget {
-  const HomePage({super.key});
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key}) : super(key: key);
 
   @override
-  State<HomePage> createState() => _HomePage();
+  State<MyHomePage> createState() => _MyHomePageState();
 }
 
-class _HomePage extends State<HomePage> {
-  // TEXT STYLE DEFINITION //
+class _MyHomePageState extends State<MyHomePage> {
+  // TEXT STYLE //
+  static const TextStyle titleText = TextStyle(
+    fontFamily: 'Montserrat',
+    color: Colors.black,
+    fontSize: 42,
+  );
   static const TextStyle headerText = TextStyle(
-      fontFamily: 'Montserrat',
-      color: Colors.black,
-      fontSize: 40,
-      fontWeight: FontWeight.w600
+    fontFamily: 'Montserrat',
+    color: Colors.black,
+    fontSize: 36,
   );
   static const TextStyle normalText = TextStyle(
-      fontFamily: 'Montserrat',
-      color: Colors.black,
-      fontSize: 24,
-      fontStyle: FontStyle.italic
+    fontFamily: 'Montserrat',
+    color: Colors.black,
+    fontSize: 20,
+    fontStyle: FontStyle.italic,
   );
 
   // DATE VARIABLES FOR CALENDAR //
-  DateTime _focusedDay = DateTime.now();
   DateTime _selectedDay = DateTime.now();
-  var _calendarFormat = CalendarFormat.month;
+  var _calendarFormat = CalendarFormat.week;
 
+  // VARIABLES FROM OTHER SITES (for the time placeholder) //
+  // ignore: non_constant_identifier_names
+  int numOfExercises = 0;
+  String workoutName = "Custom workout 1";
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text("Home")),
-      body: Column(children: [
-        Row(children: const [
-          Text("This week", style: headerText),
-        ]),
-        TableCalendar(
-          firstDay: DateTime.utc(2010, 10, 16),
-          lastDay: DateTime.utc(2030, 3, 14),
-          focusedDay: DateTime.now(),
-          selectedDayPredicate: (day) {
-            return isSameDay(_selectedDay, day);
-          },
-          onDaySelected: (selectedDay, focusedDay) {
-            setState(() {
-              _selectedDay = selectedDay;
-              _focusedDay = focusedDay;
-            });
-          },
-          calendarFormat: _calendarFormat,
-          onFormatChanged: (format) {
-            setState(() {
-              _calendarFormat = format;
-            });
-          },
-          onPageChanged: (focusedDay) {
-            _focusedDay = focusedDay;
-          },
-        ),
-
-        Row(children: const [
-          Text("Today", style: headerText),
-        ]),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: const [
-            SizedBox(
-              width: 150, height: 20,
-              child: ColoredBox(
-                color: Color(0xff5650DE),
+      body: Container(
+        width: double.infinity,
+        height: double.infinity,
+        margin: const EdgeInsets.all(10.0),
+        child: Padding(
+          padding: const EdgeInsets.only(top: 50.0, left: 25.0),
+          child: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(right: 20.0),
+                child: Column(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 5.0),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: const [Text("Body optimizer", style: titleText)],
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(top: 20),
+                      child: Row(
+                        children: const [Text("This week", style: headerText)],
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 10),
+                      child: TableCalendar(
+                        firstDay: DateTime.utc(2010, 10, 16),
+                        lastDay: DateTime.utc(2030, 3, 14),
+                        focusedDay: DateTime.now(),
+                        selectedDayPredicate: (day) {
+                          return isSameDay(_selectedDay, day);
+                        },
+                        onDaySelected: (selectedDay, focusedDay) {
+                          setState(() {
+                            _selectedDay = selectedDay;
+                          });
+                        },
+                        calendarFormat: _calendarFormat,
+                        onFormatChanged: (format) {
+                          setState(() {
+                            _calendarFormat = format;
+                          });
+                        },
+                        onPageChanged: (focusedDay) {
+                        },
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 10),
+                      child: Row(
+                        children: const [Text("Today", style: headerText)],
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 10),
+                      child: Row(
+                        // PROGRESS BAR ADD //
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 10),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [Text("$numOfExercises exercises left", style: normalText)],
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 10),
+                      child: Row(
+                        children: const [Text("Tomorrow", style: headerText)],
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 10),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [Text(workoutName, style: normalText)],
+                      ),
+                    ),
+                  ],
+                ),
               ),
-            ),
-            SizedBox(
-              width: 150, height: 20,
-              child: ColoredBox(
-                color: Colors.grey,
-              ),
-            )
-          ],
+            ],
+          ),
         ),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: const [
-            Text("0 exercises left", style: normalText),
-          ],
-        ),
-        Row(children: const [
-          Text("Tomorrow", style: headerText),
-        ]),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: const [Text("Custom Workout", style: normalText)],
-        ),
-      ]),
+      ),
     );
   }
 }
