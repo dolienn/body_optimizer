398fd1f1e0fa6280aba5718b94365efbbe3abedf
silentjacob <kubulus2004@tlen.pl>
2022-11-04 09:48:05 +0100

    NavBar working!

--- lib/app.dart
+++ lib/app.dart
@@ -1,15 +1,14 @@
+import 'package:body_optimizer/screens/mainpage.dart';
 import 'package:flutter/material.dart';
-
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
-
 
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
+      home: MainPage(),
     );
   }
 }
--- lib/screens/homepage.dart
+++ lib/screens/homepage.dart
@@ -1,9 +1,4 @@
-import 'package:persistent_bottom_nav_bar/persistent_tab_view.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:body_optimizer/screens/settings.dart';
-import 'package:body_optimizer/screens/workout.dart';
-import 'package:body_optimizer/screens/workoutcreator.dart';
 import 'package:table_calendar/table_calendar.dart';
 
 class MyHomePage extends StatefulWidget {
@@ -14,6 +9,7 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+
   // TEXT STYLE //
   static const TextStyle titleText = TextStyle(
     fontFamily: 'Montserrat',
@@ -36,51 +32,9 @@ class _MyHomePageState extends State<MyHomePage> {
   DateTime _selectedDay = DateTime.now();
   var _calendarFormat = CalendarFormat.week;
 
-  // VARIABLES FROM OTHER SITES (for the time placeholder) //
+  // VARIABLES FROM OTHER SITES (for now placeholder) //
   int numOfExercises = 0;
   String workoutName = "Custom workout 1";
-
-  // NavBar things
-  final PersistentTabController _controller =
-      PersistentTabController(initialIndex: 0);
-
-  List<Widget> _buildScreens() {
-    return const [
-      MyHomePage(),
-      Workout(),
-      WorkoutCreator(),
-      Settings(),
-    ];
-  }
-
-  List<PersistentBottomNavBarItem> _navBarsItems() {
-    return [
-      PersistentBottomNavBarItem(
-        icon: const Icon(CupertinoIcons.home),
-        title: ("Home"),
-        activeColorPrimary: CupertinoColors.activeBlue,
-        inactiveColorPrimary: CupertinoColors.systemGrey,
-      ),
-      PersistentBottomNavBarItem(
-        icon: const Icon(Icons.note_add_outlined),
-        title: ("Workout Creator"),
-        activeColorPrimary: CupertinoColors.activeBlue,
-        inactiveColorPrimary: CupertinoColors.systemGrey,
-      ),
-      PersistentBottomNavBarItem(
-        icon: const Icon(Icons.fitness_center_outlined),
-        title: ("Workout"),
-        activeColorPrimary: CupertinoColors.activeBlue,
-        inactiveColorPrimary: CupertinoColors.systemGrey,
-      ),
-      PersistentBottomNavBarItem(
-        icon: const Icon(CupertinoIcons.settings),
-        title: ("Settings"),
-        activeColorPrimary: CupertinoColors.activeBlue,
-        inactiveColorPrimary: CupertinoColors.systemGrey,
-      ),
-    ];
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -90,7 +44,7 @@ class _MyHomePageState extends State<MyHomePage> {
         height: double.infinity,
         margin: const EdgeInsets.all(10.0),
         child: Padding(
-          padding: const EdgeInsets.only(top: 50.0, left: 25.0),
+          padding: const EdgeInsets.only(top: 40.0, left: 25.0),
           child: Column(
             children: [
               Padding(
@@ -172,39 +126,6 @@ class _MyHomePageState extends State<MyHomePage> {
                     ),
                   ],
                 ),
-              ),
-              PersistentTabView(
-                context,
-                controller: _controller,
-                screens: _buildScreens(),
-                items: _navBarsItems(),
-                confineInSafeArea: true,
-                backgroundColor: Colors.white, // Default is Colors.white.
-                handleAndroidBackButtonPress: true, // Default is true.
-                resizeToAvoidBottomInset:
-                    true, // This needs to be true if you want to move up the screen when keyboard appears. Default is true.
-                stateManagement: true, // Default is true.
-                hideNavigationBarWhenKeyboardShows:
-                    true, // Recommended to set 'resizeToAvoidBottomInset' as true while using this argument. Default is true.
-                decoration: NavBarDecoration(
-                  borderRadius: BorderRadius.circular(10.0),
-                  colorBehindNavBar: Colors.white,
-                ),
-                popAllScreensOnTapOfSelectedTab: true,
-                popActionScreens: PopActionScreensType.all,
-                itemAnimationProperties: const ItemAnimationProperties(
-                  // Navigation Bar's items animation properties.
-                  duration: Duration(milliseconds: 200),
-                  curve: Curves.ease,
-                ),
-                screenTransitionAnimation: const ScreenTransitionAnimation(
-                  // Screen transition animation on change of selected tab.
-                  animateTabTransition: true,
-                  curve: Curves.ease,
-                  duration: Duration(milliseconds: 200),
-                ),
-                navBarStyle: NavBarStyle
-                    .style1, // Choose the nav bar style with this property.
               ),
             ],
           ),
