a080d0973b638dedbe99a82a51330643dba3f26e
silentjacob <kubulus2004@tlen.pl>
2022-11-04 09:48:05 +0100

    NavBar working!

---
+++ lib/screens/mainpage.dart
@@ -0,0 +1,105 @@
+import 'package:body_optimizer/screens/homepage.dart';
+import 'package:body_optimizer/screens/settings.dart';
+import 'package:body_optimizer/screens/workout.dart';
+import 'package:body_optimizer/screens/workoutcreator.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:persistent_bottom_nav_bar/persistent_tab_view.dart';
+
+class MainPage extends StatefulWidget {
+  const MainPage({Key? key}) : super(key: key);
+
+  @override
+  State<MainPage> createState() => _MainPageState();
+}
+
+class PublicVariables {
+  var activeColor = const Color(0xFF5650DE);
+}
+
+class _MainPageState extends State<MainPage> {
+  final PersistentTabController _controller =
+  PersistentTabController(initialIndex: 0);
+
+  List<Widget> _buildScreens() {
+    return [
+      const MyHomePage(),
+      const Workout(),
+      const WorkoutCreator(),
+      const Settings(),
+    ];
+  }
+
+  List<PersistentBottomNavBarItem> _navBarsItems() {
+    return [
+      PersistentBottomNavBarItem(
+        icon: const Icon(CupertinoIcons.home),
+        title: ("Home"),
+        activeColorPrimary: PublicVariables().activeColor,
+        inactiveColorPrimary: CupertinoColors.systemGrey,
+      ),
+      PersistentBottomNavBarItem(
+        icon: const Icon(Icons.fitness_center_outlined),
+        title: ("Workout"),
+        activeColorPrimary: PublicVariables().activeColor,
+        inactiveColorPrimary: CupertinoColors.systemGrey,
+      ),
+      PersistentBottomNavBarItem(
+        icon: const Icon(Icons.note_add_outlined),
+        title: ("Workout Creator"),
+        activeColorPrimary: PublicVariables().activeColor,
+        inactiveColorPrimary: CupertinoColors.systemGrey,
+      ),
+      PersistentBottomNavBarItem(
+        icon: const Icon(Icons.settings_outlined),
+        title: ("Settings"),
+        activeColorPrimary: PublicVariables().activeColor,
+        inactiveColorPrimary: CupertinoColors.systemGrey,
+      ),
+    ];
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: PersistentTabView(
+        context,
+        controller: _controller,
+        screens: _buildScreens(),
+        items: _navBarsItems(),
+        confineInSafeArea: true,
+        backgroundColor: Colors.white,
+        // Default is Colors.white.
+        handleAndroidBackButtonPress: true,
+        // Default is true.
+        resizeToAvoidBottomInset:
+        true,
+        // This needs to be true if you want to move up the screen when keyboard appears. Default is true.
+        stateManagement: true,
+        // Default is true.
+        hideNavigationBarWhenKeyboardShows:
+        true,
+        // Recommended to set 'resizeToAvoidBottomInset' as true while using this argument. Default is true.
+        decoration: NavBarDecoration(
+          borderRadius: BorderRadius.circular(10.0),
+          colorBehindNavBar: Colors.white,
+        ),
+        popAllScreensOnTapOfSelectedTab: true,
+        popActionScreens: PopActionScreensType.all,
+        itemAnimationProperties: const ItemAnimationProperties(
+          // Navigation Bar's items animation properties.
+          duration: Duration(milliseconds: 200),
+          curve: Curves.ease,
+        ),
+        screenTransitionAnimation: const ScreenTransitionAnimation(
+          // Screen transition animation on change of selected tab.
+          animateTabTransition: true,
+          curve: Curves.ease,
+          duration: Duration(milliseconds: 200),
+        ),
+        navBarStyle:
+        NavBarStyle.style4, // Choose the nav bar style with this property.
+      ),
+    );
+  }
+}
