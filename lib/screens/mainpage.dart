9e69eb48c278aea5f0e96e07817f1f1caa7e7b36
silentjacob <kubulus2004@tlen.pl>
2022-11-04 12:14:02 +0100

    Loading bar and minor changes

--- lib/screens/mainpage.dart
+++ lib/screens/mainpage.dart
@@ -19,7 +19,7 @@ class PublicVariables {
 
 class _MainPageState extends State<MainPage> {
   final PersistentTabController _controller =
-  PersistentTabController(initialIndex: 0);
+      PersistentTabController(initialIndex: 0);
 
   List<Widget> _buildScreens() {
     return [
@@ -68,18 +68,8 @@ class _MainPageState extends State<MainPage> {
         screens: _buildScreens(),
         items: _navBarsItems(),
         confineInSafeArea: true,
-        backgroundColor: Colors.white,
-        // Default is Colors.white.
-        handleAndroidBackButtonPress: true,
-        // Default is true.
-        resizeToAvoidBottomInset:
-        true,
-        // This needs to be true if you want to move up the screen when keyboard appears. Default is true.
-        stateManagement: true,
-        // Default is true.
-        hideNavigationBarWhenKeyboardShows:
-        true,
-        // Recommended to set 'resizeToAvoidBottomInset' as true while using this argument. Default is true.
+        resizeToAvoidBottomInset: true,
+        hideNavigationBarWhenKeyboardShows: true,
         decoration: NavBarDecoration(
           borderRadius: BorderRadius.circular(10.0),
           colorBehindNavBar: Colors.white,
@@ -87,18 +77,16 @@ class _MainPageState extends State<MainPage> {
         popAllScreensOnTapOfSelectedTab: true,
         popActionScreens: PopActionScreensType.all,
         itemAnimationProperties: const ItemAnimationProperties(
-          // Navigation Bar's items animation properties.
           duration: Duration(milliseconds: 200),
           curve: Curves.ease,
         ),
         screenTransitionAnimation: const ScreenTransitionAnimation(
-          // Screen transition animation on change of selected tab.
           animateTabTransition: true,
           curve: Curves.ease,
           duration: Duration(milliseconds: 200),
         ),
         navBarStyle:
-        NavBarStyle.style4, // Choose the nav bar style with this property.
+            NavBarStyle.style4,
       ),
     );
   }
