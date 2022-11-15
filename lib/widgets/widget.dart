b19909c8920b29b4173a8eb07af1acdec9704d5e
zmxncbvalsk <plawatar@gmail.com>
2022-11-15 13:10:03 +0100

    15.11.2022 11:50

--- lib/widgets/widget.dart
+++ lib/widgets/widget.dart
@@ -2,7 +2,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 Widget buildTextField(text) => Padding(
-  padding: const EdgeInsets.fromLTRB(5, 15, 5, 0),
+  padding: const EdgeInsets.symmetric(vertical: 10,horizontal: 7.5),
   child: Column(
     mainAxisAlignment: MainAxisAlignment.center,
     children: [
@@ -14,16 +14,16 @@ Widget buildTextField(text) => Padding(
 Widget buildTextField1(text)=>TextFormField(
   decoration: InputDecoration(
     enabledBorder: OutlineInputBorder(
-      borderSide: BorderSide(color: Colors.transparent),
+      borderSide: const BorderSide(color: Colors.transparent),
       borderRadius: BorderRadius.circular(5.5),
     ),
     focusedBorder: OutlineInputBorder(
-      borderSide: BorderSide(color: Colors.transparent),
+      borderSide: const BorderSide(color: Colors.transparent),
       borderRadius: BorderRadius.circular(5.5)
     ),
     hintText: text,
-    hintStyle: TextStyle(color: Colors.blue),
+    hintStyle: const TextStyle(color: Color(0xFF5650DE),),
     filled: true,
-    fillColor: Colors.blue[50],
+    fillColor: const Color(0xFF5650DE).withOpacity(0.15),
   )
 );
