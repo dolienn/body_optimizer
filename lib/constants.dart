326da5f8160e960d1943a74efc0771cab277aeca
SilentJacob <kubulus2004@tlen.pl>
2022-11-18 00:39:08 +0100

    to do - event delete!

--- lib/constants.dart
+++ lib/constants.dart
@@ -3,15 +3,19 @@ import 'package:flutter/material.dart';
 
 class PublicVariables {
   var mainColor = const Color(0xFF5650DE);
-  var accentColor = const Color(0xFFF869D5);
-  var paddingAll = const EdgeInsets.all(7.5);
-  var marginSymmetricVertical = const EdgeInsets.symmetric(vertical: 12.5);
+  var mainColor2 = const Color(0xFFF869D5);
+  var all10 = const EdgeInsets.all(10);
+  var all5 = const EdgeInsets.all(5);
+  var symmetricHorizontal = const EdgeInsets.symmetric(horizontal: 7.5);
+  var symmetricVertical = const EdgeInsets.symmetric(vertical: 12.5);
   var normalText = GoogleFonts.lato(color: Colors.black, fontSize: 14);
   var normalItalizedText = GoogleFonts.lato(
       color: Colors.black, fontSize: 16, fontStyle: FontStyle.italic);
-  var normalWhiteText = GoogleFonts.lato(color: Colors.white, fontSize: 14);
+  var normalWhiteText = GoogleFonts.lato(color: Colors.white, fontSize: 16);
   var normalGreyText = GoogleFonts.lato(color: Colors.grey, fontSize: 14);
   var normalMainColorText =
+      GoogleFonts.lato(color: const Color(0xFF5650DE), fontSize: 18);
+  var normalMainColorTextSmall =
       GoogleFonts.lato(color: const Color(0xFF5650DE), fontSize: 16);
   var subheaderText = GoogleFonts.lato(color: Colors.black, fontSize: 20);
   var headerText = GoogleFonts.lato(color: Colors.black, fontSize: 26);
@@ -20,4 +24,8 @@ class PublicVariables {
       fontSize: 34,
       fontWeight: FontWeight.w900);
   var titleText = GoogleFonts.lato(color: Colors.black, fontSize: 42);
+  var borderCircular20 = BorderRadius.circular(20);
+  var borderCircular10 = BorderRadius.circular(10);
+  var shadow = const BoxShadow(
+      color: Colors.grey, blurRadius: 2.5, offset: Offset(1, 1));
 }
